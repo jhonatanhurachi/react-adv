7a7ce8e1a6ad06508195b6dbb0c1d2fb89dc7305
Jhonatan Tupayachi Hurtado <jhonatanhurachi@gmail.com>
2022-09-11 21:48:18 -0500

    control props alternative

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -13,11 +13,7 @@ export const useProduct = ({
   value = 0,
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
-  const isControlled = useRef(!!onChange);
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ product, count: value });
-    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ product, count: newValue });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { onChangeArgs, ProductInCart } from "../interfaces/interfaces";
-import { products } from '../data/products';
+import { products } from "../data/products";
 
 export const useShoppingCart = () => {
   const [shoppingCart, setShoppingCart] = useState<{
@@ -9,24 +9,19 @@ export const useShoppingCart = () => {
 
   const onProductCountChange = ({ product, count }: onChangeArgs) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return { ...rest };
       }
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return { ...rest };
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
   return {
     shoppingCart,
     onProductCountChange,
-    products
+    products,
   };
 };
